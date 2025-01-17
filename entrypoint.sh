1f3e57a381c74b9d2d03cf608ab757de82dc7b6c
Aldoggutierrez <55823142+Aldoggutierrez@users.noreply.github.com>
2025-01-17 16:42:25 -0600

    fix: added variables to rename env file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,9 @@
 
  /app/decrypter env:decrypt --key "$KEY" --env "$ENVIRONMENT"
 
- mv /github/workspace/.env.testing /github/workspace/.env
+if [ -z "${ENVIRONMENT}" ];
+then
+    mv  "$GITHUB_WORKSPACE"/.env "$GITHUB_WORKSPACE"/.env
+else
+    mv  "$GITHUB_WORKSPACE"/.env."$ENVIRONMENT" "$GITHUB_WORKSPACE"/.env
+fi
